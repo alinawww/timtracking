2732305563fc64581f83864fe4f991485fbccf1a
alina <aliina.tudose@gmail.com>
2016-07-05 13:53:16 +0200

    created projects index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create(name:"Crazy project", description: "you won't believe it")
+Project.create(name:"Time tracking app")
+Project.create(name:"Recipes", description: "for all the food")
+Project.create(name:"Marjon", description: "girl from Amsterdam")
+Project.create(name:"Steven", description: "guy form Nigeria")
+Project.create(name:"Lorenzo", description: "guy from Barcelona")
+Project.create(name:"Valentine", description: "girl from Switzerland")
+Project.create(name:"Jorge", description: "guy from Spain")
+Project.create(name:"Miguel", description: "guy from Spain")
+Project.create(name:"Alina", description: "girl from Romania")
+Project.create(name:"Nikel", description: "guy from Germany")
+Project.create(name:"Matias", description: "guy from Italy")
