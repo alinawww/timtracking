a4bd04d68e6d7ffcf2e3a1af929b473f9f572df0
alina <aliina.tudose@gmail.com>
2016-07-06 12:48:06 +0200

    create new time entry

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -2,7 +2,27 @@ class TimeEntriesController < ApplicationController
 
   def index
     @my_project = Project.find(params[:project_id])
-    @time_entries = @my_project.time_entries.where("date > ?", 1.month.ago)
+    @time_entries = @my_project.time_entries
+  end
+  def new
+    @my_project = Project.find(params[:project_id])
+    @my_entry = @my_project.time_entries.new
+    render 'new'
+  end
+  def create
+    @my_project = Project.find(params[:project_id])
+    @my_entry = @my_project.time_entries.create(
+      hours: params[:time_entry][:hours],
+      minutes: params[:time_entry][:minutes],
+      date: params[:time_entry][:date])
+
+    if @my_entry
+      redirect_to action:'index',
+                  controller:'time_entries',
+                  project_id:@my_project.id
+    else
+      render 'new'
+    end
   end
 
 end
